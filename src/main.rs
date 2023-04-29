0a8f1e1d61f2ea7dcc8c9212e23216d5ea6e28ba
Mitch Hayes <mitchelljphayes@gmail.com>
2023-04-29 18:15:04 +1000

    added subscription endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use std::net::TcpListener;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/health-check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8008")?
-    .run()
-    .await
+    let configuration = get_configuration().expect("failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
